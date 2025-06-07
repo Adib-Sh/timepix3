d46e883048f86fdd8f0958fdc6e28c4f32bf5597
Adib Shaker <neoadib@gmail.com>
2025-06-07 13:59:24 +0200

    save file and added txt summary

--- build/FullPlot_v2.py
+++ build/FullPlot_v2.py
@@ -119,7 +119,7 @@ def plot_surface_3d(ax, xpos, ypos, zdata, title, xlabel='X Coordinate',
 setup_plot_style()
     
 # Load Data
-with h5py.File('pixel_data_20250523_111332.h5', 'r') as f:
+with h5py.File('ToTdata_datadriven_20250607_125631.h5' , 'r') as f:
     hits = f['/pixel_hits'][:]
 
 # Preprocessing
