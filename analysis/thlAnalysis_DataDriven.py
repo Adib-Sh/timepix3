df7f970c38df9dd54ab4ba079d7de100ea5dc710
Adib Shaker <neoadib@gmail.com>
2025-07-16 12:04:50 +0200

    update

--- analysis/thlAnalysis_DataDriven.py
+++ analysis/thlAnalysis_DataDriven.py
@@ -14,7 +14,7 @@ from PIL import Image
 import io
 
 
-filename = "/BeamData 20250608 NanoMAX/thlscan_datadriven_20250608_115511.h5"  # Insert filename here
+filename = "/home/adisha/git/libkatherine/build/BeamData 20250608 NanoMAX/thlscan_datadriven_20250608_115511.h5"  # Insert filename here
 
 sensor_width = 256
 sensor_height = 256
